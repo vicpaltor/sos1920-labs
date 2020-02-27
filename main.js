425a9f1e5711bbf0073421cb5feab779b1563eab
vicpaltor <victormpalostorres@gmail.com>
2020-02-27 13:31:47 +0000

    sos1920-l04

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const cool = require("cool-ascii-faces");
 cool();
 
+const express = require("express"); 
+
+var app = express();
+
+app.get("\cool",(request,response) => {
+	response.send("<html>"+cool()+"</html>");
+});
+
+app.listen(80);
+
+
+
 function main(){
 	console.log("Hello goorm!");
 }
 
 main();
 console.log(cool());
+console.log("server ready");
